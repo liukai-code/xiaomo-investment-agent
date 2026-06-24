9f70b8d54d272b8144b6a0a347af7de2cd8b1d6f
liukai-code <liukaiuup@gmail.com>
2026-06-24 23:12:15 +0800

    feat：前端页面消息格式优化

--- src/main/java/com/itlk/myclaudecode/agent/Entity/ChatMessage.java
+++ src/main/java/com/itlk/myclaudecode/agent/Entity/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.itlk.myclaudecode.agent.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ public class ChatMessage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "conversation_id", nullable = false)
     private Conversation conversation;
